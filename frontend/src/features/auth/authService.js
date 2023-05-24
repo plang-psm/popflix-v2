000345793036f0ca8151d0adac6c8402c52d8890
Phillip Sanchez <plang.psm@gmail.com>
2023-05-24 14:23:07 -0700

    update base url

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = process.env.BASE_URL || 'http://localhost:4000';
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000';
 const API_URL = `${BASE_URL}/users`;
 
 // Register user
