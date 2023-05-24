1987ce7ccb9e8fa91c2ee9fb8a547bf7716ebad9
Phillip Sanchez <plang.psm@gmail.com>
2023-05-23 23:45:29 -0700

    update serve

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ require('dotenv').config({ path: 'config.env' });
 connectDB();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../frontend')));
+// app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,6 +23,21 @@ app.use(logger('dev'));
 app.use('/', mainRoutes);
 app.use('/users', usersRoutes);
 app.use('/watchlist', watchlistRoutes);
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Popflix' });
+  });
+}
 
 app.use(errorHandler);
 app.listen(process.env.PORT, () => {
