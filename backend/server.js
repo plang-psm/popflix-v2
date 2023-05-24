6f9729324ea0e3e9e1bd6c47e0e658620a89b800
Phillip Sanchez <plang.psm@gmail.com>
2023-05-23 23:51:39 -0700

    update serve

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/users', usersRoutes);
 app.use('/watchlist', watchlistRoutes);
 
 // Serve Frontend
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
