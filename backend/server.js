5fb7d5733bb8e545ddcae98d0a2eac96ba47b005
Phillip Sanchez <plang.psm@gmail.com>
2023-05-24 12:52:34 -0700

    update file structure

--- backend/server.js
+++ backend/server.js
@@ -20,18 +20,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(logger('dev'));
 
-app.use('/', mainRoutes);
+// app.use('/', mainRoutes);
 app.use('/users', usersRoutes);
 app.use('/watchlist', watchlistRoutes);
 
 // Serve Frontend
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'development') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
   });
 } else {
   app.get('/', (_, res) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import Home from "../src/pages/Home";
-import SignUp from "../src/pages/SignUp";
-import Watchlist from "../src/pages/Watchlist"
-import Login from "../src/pages/Login";
-import Navbar from "../src/components/Navbar"
-import MoviePage from "../src/pages/MoviePage";
-import TvPage from "../src/pages/TvPage"
+import Home from '../src/pages/Home';
+import SignUp from '../src/pages/SignUp';
+import Watchlist from '../src/pages/Watchlist';
+import Login from '../src/pages/Login';
+import Navbar from '../src/components/Navbar';
+import MoviePage from '../src/pages/MoviePage';
+import TvPage from '../src/pages/TvPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Footer from "../src/components/Footer";
-import ScrollToTop from "./components/ScrollToTop";
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from "react-toastify";
+import Footer from '../src/components/Footer';
+import ScrollToTop from './components/ScrollToTop';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <div className="App bg-[black] text-[white]">
+    <div className='App bg-[black] text-[white]'>
       <Router>
         <ScrollToTop />
         <Navbar />
@@ -24,12 +24,12 @@ function App() {
           <Route path='/users/signup' element={<SignUp />} />
           <Route path='/watchlist' element={<Watchlist />} />
           <Route path='/users/login' element={<Login />} />
-          <Route path="/movie/:id" element={<MoviePage />} />
-          <Route path="/tv/:id" element={<TvPage />} />
+          <Route path='/movie/:id' element={<MoviePage />} />
+          <Route path='/tv/:id' element={<TvPage />} />
         </Routes>
         <Footer />
       </Router>
-      <ToastContainer position="top-right" />
+      <ToastContainer position='top-right' />
     </div>
   );
 }
