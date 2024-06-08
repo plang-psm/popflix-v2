a767b877dbe851ab7287dd910701560a3cf5f1aa
Phillip Sanchez <plang.psm@gmail.com>
2024-06-08 15:36:44 -0700

    test

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-      username: { type: String, unique: true },
+            username: { type: String, unique: true },
   email: { type: String, unique: true },
   password: { type: String, required: true },
 });
