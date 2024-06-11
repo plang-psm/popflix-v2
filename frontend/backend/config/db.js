adbdc212f3fbc3bd118ba8ae17d9f37bd49dd509
Phillip Sanchez <plang.psm@gmail.com>
2024-06-10 19:35:06 -0700

    rebase

--- frontend/backend/config/db.js
+++ frontend/backend/config/db.js
@@ -11,3 +11,17 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
